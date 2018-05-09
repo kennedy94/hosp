bec6efacf21d08d030395b28b1b27ecbc7e1c0b8
Kennedy Araújo <kennedyanderson94@outlook.com>
2018-05-09 13:58:00 -0300

    asdsad

--- Cplex Project/hosp.h
+++ Cplex Project/hosp.h
@@ -13,17 +13,18 @@
 #include <iomanip>
 #include <vector>
 
+#include <list>
+
 using namespace std;
 #define BIG_M 1000000
-
-
-
 
 class HOSP {
 private:
 	//dados da instancia
 	int n, l;
 	int *M, **P;
+	const char* instancia_nome;
+	class solucao;
 	IloEnv env;
 	IloModel model;
 	IloCplex cplex;
@@ -33,38 +34,38 @@ private:
 	IloArray<IloArray<IloArray<IloBoolVarArray> > > y;
 	IloArray<IloArray<IloBoolVarArray> > alpha;
 	IloArray< IloArray<IloArray<IloBoolVarArray> > > beta;
-	const char* instancia_nome;
-	
-	class solucao;
 
 	
 public:
 
+
 	//construtor para ler os arquivos
 	HOSP(const char* filename);
 
-
 	void iniciar_variaveis();
-
 
 	//lembrete: c_ eh um vetor de double
 	void funcao_objetivo();
-
-
 
 	void restricoes();
 
 	void exportar_lp();
 
 	void revolver_ppl();
+
 	void imprimir_solucao();
+
 	int maximo(int * lista, int tamanho);
+
 	//pseudo-funcao de verificacao
 	void iniciar_lp();
 
 	void imprimir_resultados(double time);
-	~HOSP();
 
+	void SPT();
+
+
+	~HOSP();
 };
 
 
